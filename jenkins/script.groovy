3bfd34210a3174dd1b556d6520283ddb31ba446c
decalnguyen <nnntoan.it@gmail.com>
2023-12-14 21:43:02 +0700

    ..

--- jenkins/script.groovy
+++ jenkins/script.groovy
@@ -1,11 +1,18 @@
 def buildServices() [
+    script{
+        def dockerImage = docker.build('server/server')
+        docker.withRegistry('http', '')
+        {
+              dockerImage.push()
+        }
+    }
     sh '''
     docker build -f Dockerfile.deploy -t devopsforiot .
     '''
 ]
 
 def git() {
-    sh
+    git credetialsId: github, url: https://github.com/decalnguyen/DevopsforIOT.git
 }
 def pushServices() {
 
